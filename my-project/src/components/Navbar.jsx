ded73e5021a9d1a1874f6b59ebb57a917812af7a
BitHero <nuhumusah874@gmail.com>
2024-01-08 22:51:13 +0000

    navbar

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import {logo} from '../assets'
+import { navLinks } from '../helpers/static';
 
 class Navbar extends Component {
   render() {
     return (
-      <nav className='relative flex justify-between items-center'>
+      <nav className='relative flex justify-between items-center p-8'>
         <div>
           <img src={logo} className='w-full h-full'/>
         </div>
 
         <div className='flex justify-between items-center'>
-          
+          <ul className='flex justify-between items-center'>
+            {navLinks.map((link) =>  
+              <li key={link.id} className=' font-poppins text-white flex justify-between'>
+                <a href={`#`}>{link.title}</a>
+              </li>
+            )}
+          </ul>
+        </div>
+
+        <div className='bg-white rounded-xl px-5 py-3'>
+          <a href='#' className=' font-poppins text-black' >Start a Project?</a>
         </div>
       </nav>
     );
---
+++ my-project/src/helpers/static.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    id: 0,
+    title: 'Milestones'
+  },
+  {
+    id: 1,
+    title: "Portfolios"
+  },
+  {
+    id: 2,
+    title: 'Services'
+  },
+  {
+    id: 3,
+    title: 'Work'
+  },
+  {
+    id: 4,
+    title: 'Testimonials'
+  }
+]
